62d1429d941fab746d97cd50467a07532d5541d0
Nur Adli <mnuradli1@gmail.com>
2018-04-12 14:17:04 +0700

    Update Script

---
+++ index.php
@@ -0,0 +1,31 @@
+<head>
+    <style type="text/css">
+        .Container
+        {
+            text-align: center;
+        }
+    
+        .Container:before
+        {
+            content: '';
+            height: 100%;
+            display: inline-block;
+            vertical-align: middle;
+        }
+    
+        .Content
+        {
+            display: inline-block;
+            vertical-align: middle;
+        }
+    </style>
+    <title>Title Here</title>
+</head>
+
+<body>
+<div class="Container">
+    <div class="Content"> /*this can be an img, span, or everything else*/
+       <center><img src="Content.png" /></center>
+    </div>
+</div>
+</body>
